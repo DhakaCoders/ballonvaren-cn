1ec036e1b1ddc588d5dd53e55c936beb0e8e47ec
Rannojit <cserannojitsarkar22@gmail.com>
2021-06-03 18:47:58 +0600

    .

--- themes/ballonvaren/header.php
+++ themes/ballonvaren/header.php
@@ -405,3 +405,28 @@
     </div>
   </div>
 </header>
+
+
+<section class="breadcrumb-sec">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="breadcrumb-cntlr">
+          <ul class="reset-list clearfix">
+            <li>
+              <a href="#">
+                <span class="item">Home</span>
+              </a>
+            </li>
+            <li>
+              <a href="#"><span>Binnenpagina</span></a>
+            </li>
+            <li class="active">
+              <span>Binnenpagina</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
