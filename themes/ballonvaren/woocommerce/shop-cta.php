ad480f2234922ab0e03b2ac78ec6293f832c18cf
haque <azizul.softsw@gmail.com>
2021-06-04 19:50:58 +0600

    login

--- themes/ballonvaren/woocommerce/shop-cta.php
+++ themes/ballonvaren/woocommerce/shop-cta.php
@@ -1,11 +1,11 @@
-<div class="clearfix"></div>
-<div class="cadeaubonnen-page-link">
-  <div class="cadeaubonnen-pg-lnk-cntrl">
-    <h4 class="fl-h4 cad-pg-lnk-title">cadeaubonnen</h4>
-    <p>Ut purus ipsum, interdum quis libero et, tincidunt tincidunt ante.</p>
-    <p>Etiam vitae ultrices purus. Praesent<br> sodales nisl vel mauris cursus viverra. Quisque ut pharetra urna. </p>
-    <a href="#" class="fl-transparent-btn cad-pg-lnk-btn">CADEAUBONNEN</a>
-  </div>
-    <i><img src="<?php echo THEME_URI; ?>/assets/images/absolute-ballon-3.svg" alt=""></i>
-</div>
-<div class="clearfix"></div>
+<li class="cadeaubonnen-page-link">
+  <div class="cadeaubonnen-page-link-bg">
+    <div class="cadeaubonnen-pg-lnk-cntrl">
+      <h4 class="fl-h4 cad-pg-lnk-title">cadeaubonnen</h4>
+      <p>Ut purus ipsum, interdum quis libero et, tincidunt tincidunt ante.</p>
+      <p>Etiam vitae ultrices purus. Praesent<br> sodales nisl vel mauris cursus viverra. Quisque ut pharetra urna. </p>
+      <a href="#" class="fl-transparent-btn cad-pg-lnk-btn">CADEAUBONNEN</a>
+    </div>
+      <i><img src="assets/images/absolute-ballon-3.svg" alt=""></i>
+  </div>    
+</li>
