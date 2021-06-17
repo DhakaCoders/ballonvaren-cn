cd4de3fb568672e2fa6eb92f80e432aa58e0b1af
haque <azizul.softsw@gmail.com>
2021-06-17 17:31:17 +0600

    contact, onze, overons

---
+++ themes/ballonvaren/page-onze-luchtballonnen.php
@@ -0,0 +1,86 @@
+<?php 
+/*Template Name: Onze Luchtballonnen*/
+get_header();
+$thisID = get_the_ID(); 
+$intro = get_field('intro', $thisID);
+$showhidegalerij = get_field('showhidegalerij', $thisID);
+$galerij = get_field('galerij', $thisID);
+$page_title = !empty($intro['titel']) ? $intro['titel'] : get_the_title();
+$sub_title = !empty($intro['subtitel']) ? '<span>'.$intro['subtitel'].'</span>' : '';
+?>
+<section class="breadcrumb-sec">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="breadcrumb-cntlr">
+          <ul class="reset-list clearfix">
+            <li>
+              <a href="#">
+                <span class="item">Home</span>
+              </a>
+            </li>
+            <li>
+              <a href="#"><span>Binnenpagina</span></a>
+            </li>
+            <li class="active">
+              <span>Binnenpagina</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="bn-masonry-gallery-sec">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <?php if($intro): ?>
+       <div class="bn-masonry-gallery-dsc-wrp">
+          <div class="page-entry-header">
+          <?php 
+            if( !empty($page_title) ) printf( '<h1 class="fl-h1">%s%s</h1>', $page_title, $sub_title );
+            if( !empty($intro['beschrijving']) ) echo wpautop( $intro['beschrijving'] );
+          ?>
+          </div>
+        </div> 
+        <?php endif; ?>
+        <?php if( $showhidegalerij ): ?>
+        <div class="bn-masonry-gallery-cntlr">
+          <?php if( $galerij ): ?>
+          <ul class="ulc reset-list bn-masonry-gallery-grid">
+            <?php foreach( $galerij as $glId ): ?>
+            <li class="bn-masonry-gallery-grid-item">
+              <div class="bn-gallery-grid-item-innr">
+                <a data-fancybox="gallery" href="<?php echo cbv_get_image_src($glId); ?>">
+                  <?php echo cbv_get_image_tag($glId, 'onzegallery'); ?>
+                </a>
+              </div>
+            </li>
+            <?php endforeach; ?>
+          </ul>
+          <?php endif; ?>
+        </div>
+        <?php endif; ?>
+      </div>
+      <?php if( $showhidegalerij ): ?>
+      <div class="col-md-12">
+        <div class="fl-pagination-ctlr">
+            <ul class="page-numbers">
+              <li><a class="prev page-numbers" href="#">←</a></li>
+              <li><span aria-current="page" class="page-numbers current">1</span></li>
+              <li><a class="page-numbers" href="#">2</a></li>
+              <li><a class="page-numbers" href="#">3</a></li>
+              <li><a class="page-numbers" href="#">4</a></li>
+              <li><span class="page-numbers dots">…</span></li>
+              <li><a class="page-numbers" href="#">15</a></li>
+              <li><a class="next page-numbers" href="#">→</a></li>
+            </ul>
+          </div>
+      </div>
+      <?php endif; ?>
+    </div>
+  </div>
+</section>
+<?php get_footer();?>
