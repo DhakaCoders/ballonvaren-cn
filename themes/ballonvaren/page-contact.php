33e6f66d62f66f7eab8f9eeaee8b5ac30566128f
haque <azizul.softsw@gmail.com>
2021-06-17 13:24:13 +0600

    page

--- themes/ballonvaren/page-contact.php
+++ themes/ballonvaren/page-contact.php
@@ -1,2 +1,176 @@
-<?php get_header();?>
+<?php 
+/*Template Name: Contact*/
+get_header();
+$thisID = get_the_ID(); 
+$intro = get_field('introsec', $thisID);
+$page_title = !empty($intro['titel']) ? $intro['titel'] : get_the_title();
+$sub_title = !empty($intro['subtitel']) ? '<span>'.$intro['subtitel'].'</span>' : '';
+$form = $intro['shortcode'];
+?>
+<section class="breadcrumb-sec">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="breadcrumb-cntlr">
+          <ul class="reset-list clearfix">
+            <li>
+              <a href="#">
+                <span class="item">Home</span>
+              </a>
+            </li>
+            <li>
+              <a href="#"><span>Binnenpagina</span></a>
+            </li>
+            <li class="active">
+              <span>Binnenpagina</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="contact-form-sec-wrp">
+ <div class="container">
+  <div class="row">
+    <div class="col-md-12">
+     <div class="contact-form-dsc-wrp">
+      <div class="page-entry-header">
+        <?php 
+          if( !empty($page_title) ) printf( '<h1 class="fl-h1">%s%s</h1>', $page_title, $sub_title );
+          if( !empty($intro['beschrijving']) ) echo wpautop( $intro['beschrijving'] );
+        ?>
+      </div>
+    </div>
+    <div class="contact-form-block clearfix">
+      <div class="contact-form-lft">
+        <div class="error-msg">
+          <span>
+            <i><svg class="error-msg-icon-svg" width="24" height="24" viewBox="0 0 24 24" fill="#ffffff">
+            <use xlink:href="#error-msg-icon-svg"></use> </svg></i>
+            Oh snap! Het formulier lijkt niet correct!</span>
+        </div>
+        <div class="contact-form-wrp clearfix">
+          <div class="wpforms-container">
+            <form class="wpforms-form needs-validation" novalidate>
+              
+              <div class="wpforms-field-container">
+                
+                <div class="wpforms-field XsNameField">
+                  <label class="wpforms-field-label">Naam</label>
+                  <input type="text" name="name" placeholder="Voornaam" required>
+                </div>
+                <div class="wpforms-field NameField">
+                  <input type="text" name="name" placeholder="Naam" required>
+                </div>
+
+                <div class="wpforms-field  FullWidthField">
+                  <label class="wpforms-field-label">telefoonnummer</label>
+                  <input type="text" name="text" placeholder="Bijv. 0493 20 36 20" required>
+                </div>
+
+                <div class="wpforms-field FullWidthField">
+                  <label class="wpforms-field-label">E-mailadres</label>
+                  <input type="email" name="email" class="form-control" required>
+                  <label id="wpforms-222-field_1-error" class="wpforms-error" for="wpforms-222-field_1">Controleer dit veld</label>
+                </div>
+
+                <div class="wpforms-field wpforms-field-textarea">
+                  <label class="wpforms-field-label">Bericht</label>
+                  <textarea name="message" placeholder="Bericht"></textarea>
+                   <div class="wpforms-field-description">
+                    <p>Wij respecteren jouw <a href="#"> privacy.</a> Jouw gegevens worden altijd vertrouwelijk behandeld.</p>
+                  </div>
+                </div>
+              </div><!-- end of .wpforms-field-container-->
+
+
+              <div class="wpforms-submit-container">
+                <button type="submit" name="submit" class="wpforms-submit">VERZENDEN</button>
+              </div>
+
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="contact-form-rgt">
+        <div class="contact-form-info-cntlr">
+          <div class="contact-form-info">
+            <h6 class="fl-h6 contact-form-info-title">Contact</h6>
+            <ul class="reset-list clearfix">
+              <li>
+                <a href="#" target="_blank">Atlantic Ballooning BVBA <br> Palingstraat 87, B-9070 Destelbergen <br> (kantoor)</a>
+              </li>
+              <li>
+                <span>Tel: <a href="tel:09 356 64 84">09 356 64 84</a></span></li>
+                <li>
+                  <span>E-mail: <a href="mailto:info@ballonvaren.be">info@ballonvaren.be</a></span>
+                </li>
+              </ul>
+            </div>
+            <div class="contact-form-info-socail has-svg-social">
+              <ul class="reset-list clearfix">
+                <li>
+                  <a href="#" target="_blank">
+                    <i><svg class="contact-facebook-icon-svg" width="24" height="24" viewBox="0 0 24 24" fill="#fff">
+                    <use xlink:href="#contact-facebook-icon-svg"></use></svg></i>
+                  </a>
+                  </li>
+                  <li>
+                    <a href="#" target="_blank">
+                      <i><svg class="contact-msg-icon-svg" width="24" height="24" viewBox="0 0 24 24" fill="#fff">
+                    <use xlink:href="#contact-msg-icon-svg"></use></svg></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" target="_blank">
+                      <i><svg class="contact-twitter-icon-svg" width="24" height="24" viewBox="0 0 24 24" fill="#fff">
+                    <use xlink:href="#contact-twitter-icon-svg"></use></svg></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" target="_blank">
+                      <i><svg class="contact-linkedin-icon-svg" width="24" height="24" viewBox="0 0 24 24" fill="#fff">
+                     <use xlink:href="#contact-linkedin-icon-svg"></use></svg></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" target="_blank">
+                      <i><svg class="contact-instagram-icon-svg" width="24" height="24" viewBox="0 0 24 24" fill="#fff">
+                    <use xlink:href="#contact-instagram-icon-svg"></use></svg></i>
+                    </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+
+<section class="contact-google-map-sec-wrp contact-white-sky-bg-cntlr">
+  <div class="contact-white-sky-bg">
+    <span class="contact-white-sky-inline-bg hide-sm" style="background: url(<?php echo THEME_URI; ?>/assets/images/contact-form-bg.png);"></span>
+    <span class="contact-white-sky-inline-bg show-sm" style="background: url(<?php echo THEME_URI; ?>/assets/images/contact-form-xs-bg.png);"></span>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="contact-google-map-wrp">
+          <div class="contact-parachute-icon">
+            <img src="<?php echo THEME_URI; ?>/assets/images/contact-parachute-icon.svg">
+          </div>
+          <div class="contact-google-map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10006.95422926144!2d2.7672152436070503!3d51.16860917329277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dca4981458e599%3A0x94ae9cf98c6fb394!2s8434%20Westende-Bad%2C%20Belgium!5e0!3m2!1sen!2sbd!4v1616587380465!5m2!1sen!2sbd" width="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+          </div> 
+          <div class="contact-google-parachute">
+            <img src="<?php echo THEME_URI; ?>/assets/images/contact-google-parachute.svg">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 <?php get_footer();?>
--- themes/ballonvaren/page.php
+++ themes/ballonvaren/page.php
@@ -22,7 +22,19 @@
     </div>
   </div>
 </section>
-
+<?php if( !is_account_page() ): ?>
+<section class="page-heading">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="breadcrumb-cntlr">
+          <h1 class="fl-h1"><?php the_title(); ?></h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php endif; ?>
 <section class="page-grd-sec-wrp">
     <?php if( is_wc_endpoint_url( 'orders' ) || is_wc_endpoint_url( 'edit-account' ) || (strpos($_SERVER['REQUEST_URI'], "winkelmandje") !== false) ){ ?>
       <div class="back-to-dashboard-btn-cntlr">
