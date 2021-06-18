b5dacd724b98ad97db291b8a9084e82259f6adbd
sabbir3109 <78914877+sabbir3109@users.noreply.github.com>
2021-06-19 00:26:08 +0600

    ...

--- themes/ballonvaren/assets/js/main.js
+++ themes/ballonvaren/assets/js/main.js
@@ -240,6 +240,50 @@ if( $('.bnProcessGridItemsSlider').length ){
 }
 }
 
+if( $('.single-product .hasRelatedProduct').length ){
+    $('.single-product .hasRelatedProduct').slick({
+      dots: false,
+      arrows:false,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      speed: 700,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+
 /*start of Niaz*/
 
 $(".type-order-format .woocommerce-input-wrapper span").each(function(){
