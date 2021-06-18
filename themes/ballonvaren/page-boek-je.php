b50bc808b6b4247269f5dbe6c553e5d9a41c86b8
haque <azizul.softsw@gmail.com>
2021-06-18 18:59:24 +0600

    review

--- themes/ballonvaren/page-boek-je.php
+++ themes/ballonvaren/page-boek-je.php
@@ -6,6 +6,7 @@ $intro = get_field('introsec', $thisID);
 $page_title = !empty($intro['titel']) ? $intro['titel'] : get_the_title();
 $sub_title = !empty($intro['subtitel']) ? '<span>'.$intro['subtitel'].'</span>' : '';
 $shortcode = get_field('shortcode', $thisID);
+$sidebar = get_field('sidebarsec', $thisID);
 ?>
 <section class="breadcrumb-sec">
   <div class="container">
@@ -38,15 +39,10 @@ $shortcode = get_field('shortcode', $thisID);
       <div class="col-md-12">
        <div class="contact-form-dsc-wrp">
         <div class="page-entry-header">
-          <h1 class="fl-h1">Contacteer ons voor meer info<span>Consectetur sit velit neque.</span></h1>
-          <p>Ornare pretium volutpat faucibus nibh suscipit dictum. Integer ut urna lacus etiam est.</p>
-          <ul class="clearfix">
-            <li>Lorem ipsum dolor sit amet.</li>
-            <li>Suspendisse faucibus.</li>
-            <li>Tortor orci turpis nunc.</li>
-          </ul>
-          <p>Eleifend pellentesque tincidunt neque, dolor. Imperdiet malesuada est feugiat quis posuere vulputate.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend pellentesque tincidunt neque, dolor. Imperdiet <br> malesuada est feugiat quis posuere vulputate sed aenean sed. </p>
+          <?php 
+            if( !empty($page_title) ) printf( '<h1 class="fl-h1">%s%s</h1>', $page_title, $sub_title );
+            if( !empty($intro['beschrijving']) ) echo wpautop( $intro['beschrijving'] );
+          ?>
         </div>
       </div>
       
@@ -60,6 +56,7 @@ $shortcode = get_field('shortcode', $thisID);
         <div class="col-md-12">
           <div class="contact-form-block clearfix">
             <div class="contact-form-lft">
+              <?php if( !empty($shortcode) ): ?>
               <div class="error-msg">
                 <span>
                   <i><svg class="error-msg-icon-svg" width="24" height="24" viewBox="0 0 24 24" fill="#ffffff">
@@ -68,100 +65,31 @@ $shortcode = get_field('shortcode', $thisID);
               </div>
               <div class="contact-form-wrp clearfix">
                 <div class="wpforms-container">
-                  <form class="wpforms-form needs-validation" novalidate>
-                    
-                    <div class="wpforms-field-container">
-                      
-                      <div class="wpforms-field XsNameField">
-                        <label class="wpforms-field-label">Naam</label>
-                        <input type="text" name="name" placeholder="Voornaam" required>
-                      </div>
-                      <div class="wpforms-field NameField">
-                        <input type="text" name="name" placeholder="Naam" required>
-                      </div>
-
-                      <div class="wpforms-field  FullWidthField">
-                        <label class="wpforms-field-label">telefoonnummer</label>
-                        <input type="text" name="text" placeholder="Bijv. 0493 20 36 20" required>
-                      </div>
-
-                      <div class="wpforms-field FullWidthField">
-                        <label class="wpforms-field-label">E-mailadres</label>
-                        <input type="email" name="email" class="form-control" required>
-                        <label id="wpforms-222-field_1-error" class="wpforms-error" for="wpforms-222-field_1">Controleer dit veld</label>
-                      </div>
-
-                      <div class="wpforms-field wpforms-field-select">
-                        <label class="wpforms-field-label" for="select1">Locatie</label>
-                        <select class="selectpicker" id="select1" name="select1">
-                          <option value="Locatie">Locatie</option>
-                          <option value="1">1</option>
-                          <option value="2">2</option>
-                          <option value="3">3</option>
-                          <option value="4">4</option>
-                          <option value="5">5</option>
-                        </select>
-                      </div>
-
-                      <div class="wpforms-field wpforms-field-select-calendar">
-                        <label class="wpforms-field-label" for="select1">DATUM</label>
-                        <input type="date" name="date" placeholder="Datum">
-                      </div>
-
-                      <div class="wpforms-field wpforms-field-select">
-                        <label class="wpforms-field-label" for="select1">Aantal volwassenen</label>
-                        <select id="select1" class="selectpicker" name="select1">
-                          <option value="Aantal volwassenen">Aantal volwassenen</option>
-                          <option value="1">1</option>
-                          <option value="2">2</option>
-                          <option value="3">3</option>
-                          <option value="4">4</option>
-                          <option value="5">5</option>
-                        </select>
-                      </div>
-
-                      <div class="wpforms-field wpforms-field-select">
-                        <label class="wpforms-field-label" for="select1">Aantal kinderen</label>
-                        <select class="selectpicker" id="select1" name="select1">
-                          <option value="Aantal kinderen">Aantal kinderen</option>
-                          <option value="1">1</option>
-                          <option value="2">2</option>
-                          <option value="3">3</option>
-                          <option value="4">4</option>
-                          <option value="5">5</option>
-                        </select>
-                      </div>
-
-                      <div class="wpforms-field wpforms-field-textarea">
-                        <label class="wpforms-field-label">extra info</label>
-                        <textarea name="message" placeholder="Extra info"></textarea>
-                        <div class="wpforms-field-description">
-                          <p>Wij respecteren jouw <a href="#"> privacy.</a> Jouw gegevens worden altijd vertrouwelijk behandeld.</p>
-                        </div>
-                      </div>
-                    </div><!-- end of .wpforms-field-container-->
-
-                    <div class="wpforms-submit-container">
-                      <button type="submit" name="submit" class="wpforms-submit">Boek je vlucht</button>
-                    </div>
-
-                  </form>
+                    <?php echo do_shortcode($shortcode); ?>
                 </div>
               </div>
+              <?php endif; ?>
             </div>
+            <?php if($sidebar): ?>
             <div class="contact-form-rgt">
               <div class="contact-form-info-cntlr">
-                  <div class="boek-parachute-icon">
-                    <img class="hide-sm" src="assets/images/boek-parachute-icon.svg">
-                    <img class="show-sm" src="assets/images/boek-parachute-icon-xs.svg">
-                  </div>
-                  <div class="contact-form-info text-center">
-                    <h4 class="fl-h4 contact-form-info-title-1">Ballonvaarten</h4>
-                    <p>Ut purus ipsum, interdum quis libero et, tincidunt tincidunt ante.</p>
-                    <a href="#" class="fl-transparent-btn">Ballonvaarten</a>
-                  </div>
+                <div class="boek-parachute-icon">
+                  <img class="hide-sm" src="<?php echo THEME_URI; ?>/assets/images/boek-parachute-icon.svg">
+                  <img class="show-sm" src="<?php echo THEME_URI; ?>/assets/images/boek-parachute-icon-xs.svg">
                 </div>
+                <div class="contact-form-info text-center">
+                  <?php 
+                    if( !empty($sidebar['titel']) ) printf('<h4 class="fl-h4 contact-form-info-title-1">%s</h4>', $sidebar['titel']);
+                    if( !empty($sidebar['beschrijving']) ) echo wpautop( $sidebar['beschrijving'] );
+                    $sknop = $sidebar['knop'];
+                    if( is_array( $sknop ) &&  !empty( $sknop['url'] ) ){
+                        printf('<a class="fl-transparent-btn" href="%s" target="%s">%s</a>', $sknop['url'], $sknop['target'], $sknop['title']); 
+                    }
+                  ?>
                 </div>
+              </div>
+              </div>
+            <?php endif; ?>
               </div>
         </div>
       </div>
