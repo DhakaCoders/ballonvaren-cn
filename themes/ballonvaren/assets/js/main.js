39dfaea716ca8252e2e29dbcc7a28b46ac239a79
Shariful700453 <56506215+Shariful700453@users.noreply.github.com>
2021-06-19 22:51:50 +0600

    ,

--- themes/ballonvaren/assets/js/main.js
+++ themes/ballonvaren/assets/js/main.js
@@ -179,7 +179,7 @@ if( $('.single-product .hasRelatedProduct').length ){
       dots: false,
       arrows:false,
       infinite: true,
-      autoplay: false,
+      autoplay: true,
       autoplaySpeed: 4000,
       speed: 700,
       slidesToShow: 3,
